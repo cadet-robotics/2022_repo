04e2334848d7fa75db55bef9c798790dd4defa21
Matthew <minerminesmc@gmail.com>
2022-01-28 19:05:29 -0500

    Actual Initial Commit >:)
    Bare bones repo
    
    Grabbed SparkMAX lib and setup some of the basic package structures and classes

---
+++ src/main/java/frc/robot/subsystems/ControlSubsystem.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ControlSubsystem extends SubsystemBase {
+    public ControlSubsystem() {
+
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+}
